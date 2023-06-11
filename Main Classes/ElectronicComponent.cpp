6a1fc31342244141b2f7cf4349f98ddee7b54abb
ilianmihov <ilian.mihov@abv.bg>
2023-06-11 20:48:46 +0300

    Update ElectronicComponent.cpp

---
+++ Main Classes/ElectronicComponent.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+enum Rotation
+{
+    
+}
+
+class ElectronicComponent
+{
+    private:
+    std::string id = "";
+    int height = 0;
+    int width = 0;
+    enum Rotation = 0;
+    //std::vector<Pin> = {};
+    //Point startingPoint;
+};
